66b798a54331b45dcf595553f5382ce651afefc2
Tina Thompson <mark_junior13@yahoo.com>
2021-10-11 12:04:06 -0500

    #winner_vertical?, no tests yet

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -18,7 +18,7 @@ class ConnectFour
   end
 
   def has_winner?
-    return winner_horizontal?
+    return winner_horizontal? || winner_vertical?
   end
 
   private
@@ -31,12 +31,28 @@ class ConnectFour
     false
   end
 
+  def winner_vertical?
+    for col_index in 0...@cols do
+      col_string = col_to_string(col_index)
+      return true if string_has_four_in_a_row?(col_string)
+    end
+    false
+  end
+
   def string_has_four_in_a_row?(str)
     str.include?(Y * 4) || str.include?(R * 4)
   end
 
   def row_to_string(row_index)
     @board[row_index].join("")
+  end
+
+  def col_to_string(col_index)
+    col_string = ""
+    for row_index in 0...@rows do
+      col_string += @board[row_index][col_index]
+    end
+    col_string
   end
 
   def place_pieces(piece_placements, start_player_index = 0) #1-based placements
